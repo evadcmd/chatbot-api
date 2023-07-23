a5c4c07414c969fb5eabee3dc9bce8d6dc59d118
TATA <evadcmd@gmail.com>
2023-07-23 18:44:40 +0900

    add prometheus metric endpoint

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from chatbot.router import streaming
 
 api = FastAPI(title="chatbot")
+Instrumentator().instrument(api).expose(api)
+
 api.include_router(streaming.router)
