f7d982d0309922b77aa181161d9a52423abbd911
TATA <evadcmd@gmail.com>
2023-07-23 17:48:25 +0900

    add log settings

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -1,3 +1,13 @@
+import logging.config
+
+import concurrent_log_handler  # pyright: ignore # noqa F401
 import dotenv
 
+from chatbot.env import CURRENT_ENV, Env
+
 dotenv.load_dotenv()
+
+logging.config.fileConfig(
+    "./log.dev.conf" if CURRENT_ENV is Env.DEV else "./log.conf",
+    disable_existing_loggers=False,
+)
